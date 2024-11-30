aa0d24e2db1e4652e4b98aed0cdb279c2c955295
Florent Sorel <florent.sorel@gmail.com>
2024-11-30 12:57:17 +0100

    initial commit

---
+++ lib/id3x.ex
@@ -0,0 +1,18 @@
+defmodule Id3x do
+  @moduledoc """
+  Documentation for `Id3x`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Id3x.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/id3x_test.exs
@@ -0,0 +1,8 @@
+defmodule Id3xTest do
+  use ExUnit.Case
+  doctest Id3x
+
+  test "greets the world" do
+    assert Id3x.hello() == :world
+  end
+end
